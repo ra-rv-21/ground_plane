d9d0d6b490026510d7024f2db9ac86ddf7bfab1e
Juan Ahumada <juan.ahumada94@gmail.com>
2021-05-06 08:45:42 -0500

    Proyecto Ground Plane Listo

---
+++ Assets/ColorButton_1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ColorButton_1 : MonoBehaviour
+{
+  private Image _colorImg;
+  private Button _colorBtn;
+
+
+  // Start is called before the first frame update
+  private void Awake()
+  {
+    _colorBtn = GetComponent<Button>();
+    _colorImg = GetComponent<Image>();
+  }
+
+  // usamos lambda ya que el metodo add listener no me permite enviar parametros,
+  // utilizando lambda "hackeamos" la funcion para que nos permita envar parametros.
+  private void Start()
+  {
+    _colorBtn.onClick.AddListener(() => GameManager.Instance.ChangeColor(_colorImg.color));
+  }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public Material materialPaint;
+
+    private void Awake(){
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ChangeColor(Color newColor){
+        materialPaint.SetColor("_Color", newColor);
+    }
+}
